04f42615c007533e4a074308265f0363b0b4c00e
Gabriel <gabriel.roblesarias@ucr.ac.cr>
2026-04-27 18:10:58 -0600

    feat: más paletas disponibles para las variables del csv

--- codigo/04_estilo.R
+++ codigo/04_estilo.R
@@ -1,18 +1,60 @@
+# 03_estilo.R
+# Estilo para los graficos
+# Autor: gabriel robles
+# Fecha: 27 04 2026
+
 # Codigo para estilo y paletas de color para graficos
 # Para utilizar usar source("codigo/04_estilo.R")
 
-#si no tiene los paquetes quitar el comentario linea 5
+#si no tiene los paquetes quitar el comentario linea 10
 #install.packages(c("cowplot", "ggplot2", "showtext"))
 
 library(ggplot2)
 library(cowplot)
 library(showtext)
 
+
+ 
+colores_sexo <- c(
+  "1" = "#01A6EA",
+  "2" = "#FFB1CB",
+)
+
+# colores factores boleanos
+# fumar, alcohol, cardio
+colores_factorriesgo <- c(
+    "0" = "#D9D9D9",  
+    "1" = "#E63946",  
+)
+
+# Lo mismo pero al reves 
+# active
+# nivel de actividad es bueno entonces se invierten
+colores_actividad <- c(
+  "1" = "#D9D9D9",  
+  "0" = "#E63946",  
+)
+
+# Para glucosa y colesterol
+colores_gluccol = c(
+  "1" = "#2A9D8F",  # Normal
+  "2" = "#E9C46A",  # Alto
+  "3" = "#F4A261"   # Muy Alto
+)
+
+# Para variables continuas como edad, ap y peso
+
+colores_default = c("#457B9D", "#1D3557") # Cool blues for density/histograms
+
+
+# colores finales para riesgo cardiovascular
 colores_riesgocardio <- c(
   "Saludable" = "#2E86AB",  
   "Riesgo Alto" = "#F24236",  
   "En Peligro" = "#F6AF65" 
 )
+
+
 
 
 # Funcion que cambia la tipografia usando cowplot. 
